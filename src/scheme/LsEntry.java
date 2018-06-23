105e52202556c5fc1606596d022f0d0cf2e52b3a
Imane NOUHI <Imane NOUHI@NETBOOK-NOUHI>
2018-06-23 19:54:14 +0000

    First commit

---
+++ src/scheme/LsEntry.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scheme;
+
+
+import org.ejml.simple.SimpleBase;
+
+/**
+ *
+ * @author Imane NOUHI
+ */
+public class LsEntry {
+    
+    SimpleBase R;
+    String Att;
+    
+}
